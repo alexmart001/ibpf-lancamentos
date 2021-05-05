74e2102be551a7949d93f04b3c7fa947b80e3d68
Alexandre Martins <alexandre.martins.001@gmail.com>
2021-05-04 23:54:34 -0300

    Carga Inicial

---
+++ src/test/java/br/com/impacta/ibpf/lancamentos/IbpfLancamentosApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.impacta.ibpf.lancamentos;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbpfLancamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
